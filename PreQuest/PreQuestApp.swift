601726468b484cb7942583f73dfff046f1f43a21
ddacora <ddacora@gmail.com>
2025-05-01 11:01:17 +0900

    Initial Commit

---
+++ PreQuest/PreQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreQuestApp.swift
+//  PreQuest
+//
+//  Created by LeeJunghun on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct PreQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PreQuestTests/PreQuestTests.swift
@@ -0,0 +1,17 @@
+//
+//  PreQuestTests.swift
+//  PreQuestTests
+//
+//  Created by LeeJunghun on 5/1/25.
+//
+
+import Testing
+@testable import PreQuest
+
+struct PreQuestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
