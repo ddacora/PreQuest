ab7ce3471a734eb03a72a1b1d8ddcf7a07dcd56e
ddacora <ddacora@gmail.com>
2025-05-02 14:01:52 +0900

    -  add NetworkMonitor

---
+++ PreQuest/Utils/NetworkMonitor.swift
@@ -0,0 +1,37 @@
+//
+//  NetworkMonitor.swift
+//  PreQuest
+//
+//  Created by LeeJunghun on 5/2/25.
+//
+
+import Foundation
+import Network
+import Combine
+
+class NetworkMonitor: ObservableObject {
+    private let monitor = NWPathMonitor()
+    private let queue = DispatchQueue(label: "NetworkMonitor")
+    @Published var isConnected = true
+    
+    init() {
+        startMonitoring()
+    }
+    
+    deinit {
+        stopMonitoring()
+    }
+    
+    private func startMonitoring() {
+        monitor.pathUpdateHandler = { [weak self] path in
+            DispatchQueue.main.async {
+                self?.isConnected = path.status == .satisfied
+            }
+        }
+        monitor.start(queue: queue)
+    }
+    
+    private func stopMonitoring() {
+        monitor.cancel()
+    }
+}
